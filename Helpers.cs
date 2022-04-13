06f0c729d48226e60c9386be166f1a98eb8016ec
Brandon Say <BrandonSay528@gmail.com>
2022-04-12 18:57:21 -0600

    initial commit for networking

---
+++ Helpers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module1Ep1
+{
+    public class Helpers
+    {
+        public static byte[] ConvertStringToBytes(string s) => System.Text.Encoding.ASCII.GetBytes(s);
+
+        public static string ConvertBytesToString(byte[] b) => System.Text.Encoding.ASCII.GetString(b);
+        public static string ConvertBytesToDisplayString(byte[] bytes , int pos , int offsett)
+        {
+            string returnString = "\n|HEX:";
+            returnString += bytes.Skip(pos).Take(offsett).Select(o => o.ToString("X2")).Aggregate((a, b) => a + ':' + b) + "|";
+            returnString += "\n|Decimal:";
+            returnString += bytes.Skip(pos).Take(offsett).Select(o => o.ToString("000")).Aggregate((a, b) => a + ':' + b) + "|";
+            returnString += "\n|ASCII:";
+            returnString += Encoding.ASCII.GetString(bytes,pos,offsett) + "|";
+            return returnString;
+            //string hstring = "";
+            //for (int i = 0; i < bytes.Length; i++)
+            //{
+            //    hstring += bytes[i].ToString("X2");
+            //    if (i < bytes.Length - 1)
+            //        hstring += ":";
+            //}
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Module1Ep1;
+using System.Net;
+using System.Net.Sockets;
+
+Console.WriteLine("Hello, World!");
+
+
+
+Task.Run(() => {
+    byte[] bytes = Helpers.ConvertStringToBytes("Welcome To The Server");
+
+    TcpListener listener = new TcpListener(IPAddress.Any, 25565);
+    listener.Start();
+    var client = listener.AcceptTcpClient();
+    var stream = client.GetStream();
+    Console.WriteLine($"Server Sent Data:{Helpers.ConvertBytesToDisplayString(bytes, 0, bytes.Length)}");
+
+    stream.Write(bytes, 0, bytes.Length);
+
+});
+
+
+Task.Run(() => {
+    byte[] bytes = new byte[255];
+
+    var client = new TcpClient();
+    client.Connect("127.0.0.1", 25565);
+    var stream = client.GetStream();
+    var readBytes = stream.Read(bytes, 0, bytes.Length);
+    Console.WriteLine($"Client Received Data:{Helpers.ConvertBytesToDisplayString(bytes, 0, readBytes)}");
+
+
+});
+
+Console.Read();
