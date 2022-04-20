f961969558ab2a69e1d4385ff9b2b56d4f3ca408
Brandon Say <BrandonSay528@gmail.com>
2022-04-20 09:42:54 -0600

    upgraded lib to use generics

--- Program.cs
+++ Program.cs
@@ -1,36 +1,97 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using Module1Ep1;
+using NetworkLib;
 using System.Net;
 using System.Net.Sockets;
 
 Console.WriteLine("Hello, World!");
 
 
-
+Helpers.LoadLogFile();
 Task.Run(() => {
-    byte[] bytes = Helpers.ConvertStringToBytes("Welcome To The Server");
 
     TcpListener listener = new TcpListener(IPAddress.Any, 25565);
     listener.Start();
-    var client = listener.AcceptTcpClient();
-    var stream = client.GetStream();
-    Console.WriteLine($"Server Sent Data:{Helpers.ConvertBytesToDisplayString(bytes, 0, bytes.Length)}");
+    int connectionId = 0;
+    while (true) {
+        var client = listener.AcceptTcpClient();
+        var connection = new TcpConnection<PacketIdentifier>(connectionId, client);
+        connection.OnConnected += (s, e) => {
+            Console.WriteLine($@"Client connected");
 
-    stream.Write(bytes, 0, bytes.Length);
+            Task.Run(() =>
+            {
+                int x = 0; int y = 0; int z = 0;
+                while (true)
+                {
+                    y++;
 
+                    var movePacket = new WritePacket<PacketIdentifier>(PacketIdentifier.Position);
+                    movePacket.WriteInt(x);
+                    movePacket.WriteInt(y);
+                    movePacket.WriteInt(z);
+                    e.WritePacket(movePacket);
+                    Thread.Sleep(150);
+                }
+
+            });
+
+
+            //var packet = new WritePacket<PacketIdentifier>(PacketIdentifier.Welcome);
+            //packet.WriteString($@"Welcome to the server");
+            //e.WritePacket(packet);
+        };
+        Task.Run(async () => 
+        await connection.ConnectAsync());
+    }
 });
 
 
 Task.Run(() => {
     byte[] bytes = new byte[255];
 
-    var client = new TcpClient();
-    client.Connect("127.0.0.1", 25565);
-    var stream = client.GetStream();
-    var readBytes = stream.Read(bytes, 0, bytes.Length);
-    Console.WriteLine($"Client Received Data:{Helpers.ConvertBytesToDisplayString(bytes, 0, readBytes)}");
+    var connection = new TcpConnection<PacketIdentifier>(0,"127.0.0.1",25565);
+    connection.PacketReceived += (s, e) => {
+        Console.Write($@"Packet Received({e.PacketId})");
+        if(e.PacketId == PacketIdentifier.Position)
+        {
+            int x, y, z;
+            x = e.Packet.ReadInt();
+            y = e.Packet.ReadInt();
+            z = e.Packet.ReadInt();
+            Console.Write($"({x},{y},{z})\n");
+        }
+        //else if ( e.PacketId == PacketIdentifier.Welcome)
+        //{
+        //    string message = e.Packet.ReadString();
+        //    Console.Write($"\"{message}\"\n");
+        //}
 
+        else
+        {
+            Console.Write($"RAW({Helpers.ConvertPacketEventToTextData(e)})\n");
+        }
+    };
+    Task.Run(async () =>
+       await connection.ConnectAsync());
+
+
+});
+Task.Run(() => {
+    while (true)
+    {
+        Helpers.SaveLogTick();
+        Thread.Sleep(500);
+    }
 
 });
 
 Console.Read();
+
+
+public enum PacketIdentifier
+{
+    None=0,
+    Welcome=1,
+    Position=2
+}
+
